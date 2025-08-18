41ff945f8b36991d643c9bda8a6c9e27ac4ad0d8
Daniel444777 <mahechadaniel54@gmail.com>
2025-08-17 21:04:50 -0500

    Add files via upload

---
+++ Módulo 7 Funciones Daniel Mahecha/Ej6.py
@@ -0,0 +1,5 @@
+def potencia(base, exponente=2):
+    return base ** exponente
+
+print(potencia(5)) 
+print(potencia(5, 3))
---
+++ Módulo 7 Funciones Daniel Mahecha/Ej7.py
@@ -0,0 +1,5 @@
+multiplicar = lambda a, b: a * b
+print(multiplicar(4, 6))
+
+nombres = ["Juan", "Ana", "Pedro", "Sofia"]
+print(sorted(nombres, key=lambda x: len(x)))
