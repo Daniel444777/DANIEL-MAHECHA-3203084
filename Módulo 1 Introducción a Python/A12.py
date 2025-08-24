c541205b5715ef8571db4e062f1f07e6a1af5563
Daniel444777 <mahechadaniel54@gmail.com>
2025-08-24 16:55:50 -0500

    Add files via upload

---
+++ Módulo 1 Introducción a Python/A12.py
@@ -0,0 +1,11 @@
+estudiante = {
+    "nombre": "Ana",
+    "edad": 21,
+    "carrera": "Ingeniería"
+}
+
+print("Edad del estudiante:", estudiante["edad"])
+
+
+estudiante["universidad"] = "Universidad Nacional"
+print("Diccionario actualizado:", estudiante)
