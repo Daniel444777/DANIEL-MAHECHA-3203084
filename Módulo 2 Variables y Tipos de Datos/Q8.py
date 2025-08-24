8417e596ae451329771b91255e38d18ede844382
Daniel444777 <mahechadaniel54@gmail.com>
2025-08-24 17:13:30 -0500

    Add files via upload

---
+++ Módulo 2 Variables y Tipos de Datos/Q8.py
@@ -0,0 +1,17 @@
+dic1 = {"Nombre": "Manuel", "nota1":45,"nota2":70,"nota3":70,"nota4":75,"nota5":95 }
+dic2 = {"Nombre": "Sergio", "nota1":85,"nota2":90,"nota3":70,"nota4":45,"nota5":80 }
+dic3 = {"Nombre": "Camilo", "nota1":60,"nota2":80,"nota3":40,"nota4":88,"nota5":90 }
+dic4 = {"Nombre": "Santiago", "nota1":10,"nota2":10,"nota3":100,"nota4":79,"nota5":95 }
+dic5 = {"Nombre": "Adnres", "nota1":10,"nota2":100,"nota3":85,"nota4":80,"nota5":100 }
+
+promedio1 = {dic1["nota1"]+dic1["nota2"]+dic1["nota3"]+dic1["nota4"]+dic1["nota5"]}/5
+promedio2 = {dic2["nota1"]+dic2["nota2"]+dic2["nota3"]+dic2["nota4"]+dic2["nota5"]}/5
+promedio3 = {dic3["nota1"]+dic3["nota2"]+dic3["nota3"]+dic3["nota4"]+dic3["nota5"]}/5
+promedio4 = {dic4["nota1"]+dic4["nota2"]+dic4["nota3"]+dic4["nota4"]+dic4["nota5"]}/5
+promedio5 = {dic5["nota1"]+dic5["nota2"]+dic5["nota3"]+dic5["nota4"]+dic5["nota5"]}/5
+
+print ("el promedio de Manuel es:", promedio1)
+print ("el promedio de Sergio es:", promedio2)
+print ("el promedio de Camilo es:", promedio3)
+print ("el promedio de Santiago es:", promedio4)
+print ("el promedio de Andres es:", promedio5) 
